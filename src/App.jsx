8ad85171a6d864a564052c5d9636939b27f0e4ae
Sascha Rose <sascha.rose@gmail.com>
2025-03-27 14:47:21 +0100

    init sample

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+function App() {
+  return (
+    <>
+      <div className="flex">
+        <div className="h-20 w-20 bg-amber-200 flex justify-center items-center">
+          1
+        </div>
+        <div className="h-20 w-20 bg-amber-600 flex justify-center items-center">
+          2
+        </div>
+      </div>
+      <div className="w-80">
+        <img src="card.png" />
+      </div>
+    </>
+  );
+}
+
+export default App;
